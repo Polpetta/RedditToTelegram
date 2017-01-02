fa8e3607b2f7907a62d9aa14d6d4ad8040fec146
Davide Polonio <poloniodavide@gmail.com>
2017-01-02 15:59:02 +0100

    First draft of the Reddit Controller -The controller will subscribe to the events of the view and it'll check if there are new posts. It'll use Redis in order to store and retrive the data.

--- src/reddit/redditController.js
+++ src/reddit/redditController.js
@@ -1,3 +1,33 @@
 /**
  * Created by davide on 01/01/17.
  */
+
+import {RedditView} from './redditView'
+import EventEmitter from 'events'
+import RedisClient from 'redis'
+
+export class RedditController extends EventEmitter {
+  constructor (subredditName, pollingTime) {
+
+    super()
+    this.view = new RedditView(subredditName, pollingTime)
+    this.redis = new RedisClient()
+  }
+
+  checkNews (listOfNews) {
+    /*
+     * TODO: integration with redis database. The idea is to save all the
+     * keys of the post and check in the database if there are new keys. I
+     * will probably use a set. It's faster than using a simple txt.
+     *
+     */
+  }
+
+  getNewPosts () {
+    const self = this
+    this.view.on('newPosts', function (listOfNews) {
+      self.checkNews(listOfNews)
+    })
+    this.view.startPolling()
+  }
+}
