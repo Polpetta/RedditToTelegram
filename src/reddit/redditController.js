bbb264edea0bbabc30283b7a7240544f7cbc6707
Davide Polonio <poloniodavide@gmail.com>
2017-01-03 16:35:59 +0100

    Add integration with redis. -Now the controller is able to connect to a redis server and send the id in a set (with the name of the subreddit).

--- src/reddit/redditController.js
+++ src/reddit/redditController.js
@@ -5,13 +5,24 @@
 import {RedditView} from './redditView'
 import EventEmitter from 'events'
 import RedisClient from 'redis'
+import {redisConfig} from '../utils/config'
 
 export class RedditController extends EventEmitter {
-  constructor (subredditName, pollingTime) {
 
+  constructor (subredditName, pollingTime) {
     super()
+    this.subredditName = subredditName
     this.view = new RedditView(subredditName, pollingTime)
-    this.redis = new RedisClient()
+    // Problems with Standard style with "createClient" method
+    this.redis = new RedisClient.createClient(redisConfig.getConfig()) // eslint-disable-line
+
+    this.redis.on('error', function (err) {
+      // This error happen when using the bot with docker, so for the moment
+      // I'll hide it.
+      if (err !== 'ReplyError: ERR invalid DB index') {
+        console.log('Error ' + err)
+      }
+    })
   }
 
   checkNews (listOfNews) {
@@ -19,8 +30,21 @@ export class RedditController extends EventEmitter {
      * TODO: integration with redis database. The idea is to save all the
      * keys of the post and check in the database if there are new keys. I
      * will probably use a set. It's faster than using a simple txt.
+     * If there are new entries I should emit an event and send the new posts
+     * to Telegram
      *
      */
+
+    const self = this
+    listOfNews.then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        console.log('id: ' + data[i].id)
+        console.log('Adding a new entry...')
+        self.redis.sadd(self.subredditName, data[i].id, function () {
+          console.log('New entry ' + data[i].id + ' added')
+        })
+      }
+    })
   }
 
   getNewPosts () {
