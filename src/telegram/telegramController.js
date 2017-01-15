97591be273584aaff273366500643e277a5c8234
Davide Polonio <poloniodavide@gmail.com>
2017-01-15 15:28:41 +0100

    Add support for new subscribers and ability to send messages

--- src/telegram/telegramController.js
+++ src/telegram/telegramController.js
@@ -4,16 +4,28 @@
 
 import {TelegramView} from './telegramView'
 import {TelegramModel} from './telegramModel'
+import EventEmitter from 'events'
 
-export class TelegramController {
+export class TelegramController extends EventEmitter {
 
   constructor () {
-    this.model = new TelegramModel ()
+    super()
+
+    const self = this
+    this.model = new TelegramModel()
     this.view = new TelegramView(this.model)
 
+    // Events
+    this.view.on('addedToANewGroup', function (id) {
+      self.emit('newSubscriber', id)
+    })
   }
 
-  pushATextMessage(id, message) {
-    this.model.sendMessage(id, message)
+  pushATextMessage (id, message) {
+    const toSend = 'New post from ' + message.domain + '! \n' +
+      'Title: ' + message.title + '\n' +
+      'Link: ' + message.url
+
+    this.model.sendMessage(id, toSend)
   }
 }
